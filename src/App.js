b8659908ada62b2a21f4f2748c0ac90e6c71963e
RobDLev <75288391+RobDLev@users.noreply.github.com>
2021-04-24 17:30:39 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <div className="container">
+        <p>Hello World</p>
+        <div>
+          <a href="https://github.com/RobDLev" target="_blank" rel="noreferrer">
+            Open-source code{" "}
+          </a>
+          by Robin Davis; Hosting by
+          <a href="https://netlify.com" target="_blank" rel="noreferrer">
+            Netlify
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
