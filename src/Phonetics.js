0a2d122a46a955200268b6d965fa3f1f3f85e231
RobDLev <75288391+RobDLev@users.noreply.github.com>
2022-08-01 11:57:45 -0700

    Updated Phonetics. js to only show audio button if audio url is present

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  function playAudio() {
-    let audio = new Audio(props.phonetics.audio);
-    audio.play();
-  }
+  if (props.phonetics.audio) {
+    function playAudio() {
+      let audio = new Audio(props.phonetics.audio);
+      audio.play();
+    }
 
-  return (
-    <div className="Phonetics">
-      <h4>
-        {props.phonetics.text}
-        <button className="speakerButton" onClick={playAudio}>
-          🔈
-        </button>
-      </h4>
-    </div>
-  );
+    return (
+      <div className="Phonetics">
+        <h4>
+          {props.phonetics.text}
+          <button className="speakerButton" onClick={playAudio}>
+            🔈
+          </button>
+        </h4>
+      </div>
+    );
+  }    
 }
