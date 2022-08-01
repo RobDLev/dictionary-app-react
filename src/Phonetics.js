a8042f573979de2d983344334a77d45a1947da21
RobDLev <75288391+RobDLev@users.noreply.github.com>
2022-08-01 12:01:24 -0700

    Update Phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  if (props.phonetics.audio) {
+  if (props.phonetics.audio != "") {
     function playAudio() {
       let audio = new Audio(props.phonetics.audio);
       audio.play();
