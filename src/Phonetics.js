f19f52cebaf9123032c67ceb0c130c7ae8f5f188
RobDLev <75288391+RobDLev@users.noreply.github.com>
2022-08-04 14:17:45 -0700

    Updated Phonetics.js, fixed audio button issue

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,15 +6,23 @@ export default function Phonetics(props) {
     let audio = new Audio(props.phonetics.audio);
     audio.play();
   }
-  
-  return (
-    <div className="Phonetics">
-      <h4>
-        {props.phonetics.text}
-        <button className="speakerButton" onClick={playAudio}>
-          🔈
-        </button>
-      </h4>
-    </div>
-  );    
+
+  if (props.phonetics.audio !== "") {
+    return (
+      <div className="Phonetics">
+        <h4>
+          {props.phonetics.text}
+          <button className="speakerButton" onClick={playAudio}>
+            🔈
+          </button>
+        </h4>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Phonetics">
+        <h4>{props.phonetics.text}</h4>
+      </div>
+    );
+  }
 }
