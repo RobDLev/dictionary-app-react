2ba9babcdb1897b497a24d96c988c783cbb65b8d
Robin Levinson <75288391+RobDLev@users.noreply.github.com>
2023-03-03 13:21:03 -0800

    Added dark mode toggle

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
     <div className="App">
       <div className="container">
         <main>
+          <div className="darkmode"> 
+            <i id="toggle_off" class="bi bi-toggle-off"></i> Dark Mode Off
+          </div>
           <h1>
             <div className="Antonio">Schmirriam-Schwebster</div>
             <div className="AlfaSlabOne">Dictionary</div>
